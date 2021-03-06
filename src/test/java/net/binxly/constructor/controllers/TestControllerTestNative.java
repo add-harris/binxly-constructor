a6aaaef38e1234a13d02bf3b23c26fc3fa38ebc0
adam.harrison <adam.harrison1@dwp.gsi.gov.uk>
2021-03-06 22:03:06 +0000

    deleted native TestController test

--- src/test/java/net/binxly/constructor/controllers/TestControllerTestNative.java
+++
@@ -1,7 +0,0 @@
-package net.binxly.constructor.controllers;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class TestControllerTestNative extends TestControllerTest {
-}
