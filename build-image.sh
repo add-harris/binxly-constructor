59a217a00ff241fda1672c25f6ddb64a472d0308
adam.harrison <adam.harrison1@dwp.gsi.gov.uk>
2021-03-06 22:16:08 +0000

    changed gitignore file to push built runner to github

--- build-image.sh
+++ build-image.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+$HOME/bin/apache-maven-3.6.3/bin/mvn package -Pnative -Dquarkus.native.container-build=true
 docker build -f src/main/docker/Dockerfile.distroless -t binxly/constructor .
