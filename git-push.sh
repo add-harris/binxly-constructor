1a3462d985fabe38eec6f1be8dda670e4104d699
adam.harrison <adam.harrison1@dwp.gsi.gov.uk>
2021-03-07 11:22:18 +0000

    #./build-exec.sh # #git add -f target/constructor-1.0.0-SNAPSHOT-runner

--- git-push.sh
+++ git-push.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
 
-./build-exec.sh
-git push
+# capture last git message
+message=$(git log -1 --oneline)
+# trim off git ref (first 8 chars)
+message=${message:8}
+
+echo "message captured: ${message}"
+
+#./build-exec.sh
+#git add -f target/constructor-1.0.0-SNAPSHOT-runner
+
+git add .
+git cmt "temp commit"
+git reset --soft HEAD~2
+git cmt "${message}"
+
+#git cmt "fix-up"
+#git reset --soft HEAD~2
+#git cmt $message
+#git push
