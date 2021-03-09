be7c90d6ba753ad1fd3f1cbb00ae6519eb413e2a
adam.harrison <adam.harrison1@dwp.gsi.gov.uk>
2021-03-09 20:59:19 +0000

    corrected new component annotations

--- src/main/java/net/binxly/constructor/models/components/ImageBoxComponent.java
+++ src/main/java/net/binxly/constructor/models/components/ImageBoxComponent.java
@@ -1,12 +1,5 @@
 package net.binxly.constructor.models.components;
 
-import lombok.Data;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class ImageBoxComponent extends Component {
 
 }
